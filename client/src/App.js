48232ec3d212d192ce54d966983de9e1ab452e2b
Dawson Coombs <dawsontech0@gmail.com>
2023-01-11 20:20:06 -0500

    Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Dawson</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
