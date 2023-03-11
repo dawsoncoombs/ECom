a0102ef285060edcc19b78c3126ea3db4d524037
Dawson Coombs <dawsontech0@gmail.com>
2023-03-11 17:30:17 -0500

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
-import data from "./data.js";
+// import data from "./data.js";
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./Views/homeScreen";
 import ProductScreen from "./Views/productScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to='/'>Dawson</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+              <Navbar.Brand>daw's Sins</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path='/' element={<HomeScreen/>} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen/>} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
